f6308f86128663409037c10e69814f3813dc4892
jinxuefeng <jin-xue-feng@163.com>
2016-10-19 11:29:19 +0800

    使用内部内实现单例，既可以做到延迟加载也不用synchronized关键字，是一个比较完善的实现

--- src/com/test/single/SingletonUpdate.java
+++ src/com/test/single/SingletonUpdate.java
@@ -12,6 +12,9 @@ public class SingletonUpdate {
 	/**
 	 * 内部类：内部类在SingletonUpdate加载的时候并不会加载
 	 * 	确保SingletonUpdate被JVM加载时，不会初始化单例类
+	 * 
+	 * 	使用内部内实现单例，既可以做到延迟加载也不用synchronized关键字，
+	 *  是一个比较完善的实现
 	 * @author Administrator
 	 *
 	 */
