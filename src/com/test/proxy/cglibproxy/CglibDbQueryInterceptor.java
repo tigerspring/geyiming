1dd873caf74498004944bb2587083db3e6fd7b80
jinxuefeng <jin-xue-feng@163.com>
2016-10-28 16:51:59 +0800

    cglib动态代理类

--- src/com/test/proxy/cglibproxy/CglibDbQueryInterceptor.java
+++ src/com/test/proxy/cglibproxy/CglibDbQueryInterceptor.java
@@ -15,6 +15,9 @@ public class CglibDbQueryInterceptor implements MethodInterceptor{
 
 	@Override
 	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+		/*
+		 *	代理类的内部逻辑 
+		 */
 		if(real == null){
 			real = new DBQuery();
 		}
@@ -23,9 +26,9 @@ public class CglibDbQueryInterceptor implements MethodInterceptor{
 	
 	public static IDBQuery createCglibProxy(){
 		Enhancer enhancer = new Enhancer();
-		enhancer.setCallback(new CglibDbQueryInterceptor());
-		enhancer.setInterfaces(new Class[]{IDBQuery.class});
-		IDBQuery cglibproxy = (IDBQuery) enhancer.create();
+		enhancer.setCallback(new CglibDbQueryInterceptor());//指定切入器,定义代理逻辑类
+		enhancer.setInterfaces(new Class[]{IDBQuery.class});//指定实现的接口
+		IDBQuery cglibproxy = (IDBQuery) enhancer.create();//生成代理类的实例
 		return cglibproxy;
 	}
 
